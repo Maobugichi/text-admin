15246c68f56e6eb382a5f96862a16b9168f827f7
Maobugichi <immansamuel846@gmail.com>
2025-05-23 15:02:19 -0700

    update ad posts

--- src/components/postimg.tsx
+++ src/components/postimg.tsx
@@ -1,43 +1,85 @@
 import { useState } from "react"
-import Form from "./form"
-import Input from "./input"
-import { handleFormSubmit } from "../utils"
-import Button from "./button"
 import { handleChange } from "../utils"
-const PostImg = () => {
-    const [ data , setData ] = useState<any>({
+import Input from "./input"
+import Form from "./form"
+import Button from "./button"
+import axios from "axios"
+
+const PostImage = () => {
+    const [ data,setData ] = useState<any>({
         identifier:'',
-        image_url:'',
         content:'',
         link:''
-    });
+    })
+    const [file, setFile] = useState<any>(null);
+    const [preview, setPreview] = useState<any>('');
     const [ showLoader ,  setShowLoader ] = useState<boolean>(false);
+
+   const handleFileChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+   
+    if (!files || files.length === 0) return;
+    const file = Array.from(files);
+     console.log(file[0])
+    setFile(file);
+    setPreview(file.map((file) => URL.createObjectURL(file)));
+   };
     
+   
+  const handleUpload = async (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (!file) return alert('No file selected');
+
+    const formData = new FormData();
+    file.forEach((file:any) => formData.append('images', file));
+     formData.append('content', data.content);
+    formData.append('link', data.link);
+    formData.append('identifier', data.identifier); 
+    try {
+        console.log(formData)
+        setShowLoader(true)
+         const res = await axios.put('https://textflex-axd2.onrender.com/api/admin-img', formData,{
+        headers: {
+          'Content-Type': 'multipart/form-data'
+          ,
+        },
+      });
+       console.log(res)
+       setData({
+         identifier:'',
+        content:'',
+        link:''
+       })
+       setFile(null)
+       setPreview('')
+      
+      setShowLoader(false)
+      alert('Image uploaded and saved!');
+    } catch (err) {
+      console.error(err);
+      alert('Upload failed');
+    }
+  };
     return(
         <Form
-         onSubmit={(e) => { handleFormSubmit({e,data,setShowLoader,setData,endpoint: 'api/admin-img',method:'POST', 
-            onSuccess: (res) => {
-                console.log('Form success:', res);
-            },
-            onError: (err) => {
-                console.log('Form error:', err);
-            },
-            })}}
-         header="Enter image Url"
-         height="h-[250px]"
+         onSubmit={handleUpload}
+         header="post ad"
+         height='h-fit min-h-[420px]'    
         >
-              <Input placeholder="enter identifier" name="content" value={data.content} type="text"  handleChange={(e) => handleChange(e, setData)}/>
               <Input placeholder="enter text" name="content" value={data.content} type="text"  handleChange={(e) => handleChange(e, setData)}/>
-              <Input placeholder="enter img url" name="image_url" value={data.image_url} type="text"  handleChange={(e) => handleChange(e, setData)}/>
-              <Input placeholder="enter link" name="link" value={data.image_url} type="text"  handleChange={(e) => handleChange(e, setData)}/>
-              <Input placeholder="enter img id" name="id" value={data.id} type="text"  handleChange={(e) => handleChange(e, setData)}/>
-            <Button
-             content="Submit"
-             type="submit"
-             showLoader={showLoader}
-            />
+              <Input placeholder="enter identifier" name="identifier" value={data.identifier} type="text"  handleChange={(e) => handleChange(e, setData)}/>
+                <div>
+                   {preview !== '' && <img src={preview} alt="ad img" /> }
+                </div>
+                <input className="bg-white w-[90%] border border-solid border-gray-400 rounded-md mx-auto" type="file" accept="image/*" onChange={handleFileChange} />
+              <Input placeholder="enter link" name="link" value={data.link} type="text"  handleChange={(e) => handleChange(e, setData)}/>
+              <Button
+               content="Submit"
+               showLoader={showLoader}
+               type="submit"
+              />
         </Form>
     )
 }
 
-export default PostImg
+export default PostImage
--- src/components/updateimage.tsx
+++ src/components/updateimage.tsx
@@ -1,36 +1,77 @@
 import { useState } from "react"
-import { handleChange , handleFormSubmit } from "../utils"
+import { handleChange } from "../utils"
 import Input from "./input"
 import Form from "./form"
 import Button from "./button"
+import axios from "axios"
 
 const UpdateImage = () => {
     const [ data,setData ] = useState<any>({
         identifier:'',
-        image_url:'',
         content:'',
         link:''
     })
+    const [file, setFile] = useState<any>(null);
+    const [preview, setPreview] = useState<any>('');
+  
     const [ showLoader ,  setShowLoader ] = useState<boolean>(false);
 
+   const handleFileChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+   
+    if (!files || files.length === 0) return;
+    const file = Array.from(files);
+     console.log(file[0])
+    setFile(file);
+    setPreview(file.map((file) => URL.createObjectURL(file)));
+   };
     
+   
+  const handleUpload = async (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (!file) return alert('No file selected');
+
+    const formData = new FormData();
+    file.forEach((file:any) => formData.append('images', file));
+     formData.append('content', data.content);
+    formData.append('link', data.link);
+    formData.append('identifier', data.identifier); 
+    try {
+        console.log(formData)
+        setShowLoader(true)
+         const res = await axios.post('https://textflex-axd2.onrender.com/api/admin/update', formData,{
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+       console.log(res)
+        setData({
+         identifier:'',
+        content:'',
+        link:''
+       })
+       setFile(null)
+       setPreview('')
+      setShowLoader(false)
+      alert('Image uploaded and saved!');
+    } catch (err) {
+      console.error(err);
+      alert('Upload failed');
+    }
+  };
     return(
         <Form
-         onSubmit={(e) => { handleFormSubmit({e,data,setShowLoader,setData,endpoint: 'api/admin/update', method:"PATCH", 
-            onSuccess: (res) => {
-                console.log('Form success:', res);
-            },
-            onError: (err) => {
-                console.log('Form error:', err);
-            },
-            })}}
-         header="update image url"
-         height='h-[320px]'    
+         onSubmit={handleUpload}
+         header="update ad"
+         height='h-fit min-h-[420px]'    
         >
               <Input placeholder="enter text" name="content" value={data.content} type="text"  handleChange={(e) => handleChange(e, setData)}/>
-              <Input placeholder="enter img url" name="image_url" value={data.image_url} type="text"  handleChange={(e) => handleChange(e, setData)}/>
-              <Input placeholder="enter link" name="link" value={data.image_url} type="text"  handleChange={(e) => handleChange(e, setData)}/>
-              <Input placeholder="enter img id" name="id" value={data.id} type="text"  handleChange={(e) => handleChange(e, setData)}/>
+              <Input placeholder="enter identifier" name="identifier" value={data.identifier} type="text"  handleChange={(e) => handleChange(e, setData)}/>
+                <div>
+                     {preview !== '' && <img src={preview} alt="ad img" /> }
+                </div>
+                <input className="bg-white w-[90%] border border-solid border-gray-400 rounded-md mx-auto" type="file" accept="image/*" onChange={handleFileChange} />
+              <Input placeholder="enter link" name="link" value={data.link} type="text"  handleChange={(e) => handleChange(e, setData)}/>
               <Button
                content="Submit"
                showLoader={showLoader}
