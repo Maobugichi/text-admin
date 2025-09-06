62c83933698db7f7708136403a46e5078186d02a
Maobugichi <immansamuel846@gmail.com>
2025-09-06 12:01:05 +0100

    adjust logout btn

--- src/components/nav/sideNav.tsx
+++ src/components/nav/sideNav.tsx
@@ -37,7 +37,7 @@ const SideNav:React.FC<SideNavProps> = ({show , setIsShow }) => {
                 initial={{x: -500 }}
                 animate={show ? { x: [-500, -300, 0] } : {}}
                 exit={{ x: -500}}
-                className={`fixed z-20 w-[60%] md:w-[20%] md:fixed h-[100vh] top-0 md:flex flex-col gap-5  border-r border-solid  ${theme ? 'bg-[#242424] border-blue-100' : 'bg-[#f9fbfd] border-[#5252]'}`}>
+                className={`fixed z-20 w-[60%] md:w-[20%] md:fixed h-screen top-0 md:flex flex-col gap-5  border-r border-solid  ${theme ? 'bg-[#242424] border-blue-100' : 'bg-[#f9fbfd] border-[#5252]'}`}>
                    <div className={`h-16 flex items-center justify-end pr-4 border-b border-solid border-[#5252] ${theme ? 'border-blue-100': 'border-[#5252]'}`}>
                        <X onClick={closeNav} className="md:hidden"/>
                    </div>
@@ -46,10 +46,12 @@ const SideNav:React.FC<SideNavProps> = ({show , setIsShow }) => {
                         <NavItems
                         closeNav={closeNav}
                         />
+                         <Link className="bg-red-600 rounded-full  w-10 h-10  ml-42 grid place-items-center " to="/">
+                            <LogOutIcon className="text-white"/>
+                        </Link>
                     </div>
-                     <Link className="bg-red-600 rounded-full  w-10 h-10  ml-42 grid place-items-center " to="/">
-                        <LogOutIcon className="text-white"/>
-                    </Link>
+
+                    
                </motion.nav>) :
                 (<nav
                 className={`fixed z-20 md:w-[20%] h-[100vh] top-0 md:flex flex-col gap-5  border-r border-solid  ${theme ? 'bg-[#242424] border-blue-100' : 'bg-[#f9fbfd] border-[#5252]'}`}>
